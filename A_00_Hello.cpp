9380b967babd314fd38620ea327400217a3c88ee
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2024-11-07 18:24:03 -0600

    Create A_00_Hello.cpp

---
+++ A_00_Hello.cpp
@@ -0,0 +1,25 @@
+/***************************************************************
+CSCI 240         Program 0     Spring 2024
+
+Name: Avi Schmookler Z2005237
+
+Section: 02
+
+Date Due: Friday, January 19
+
+Purpose: Compiler training
+***************************************************************/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{    
+    cout << "\nhello, world";
+    
+    cout << "\ngoodbye for now..";
+
+    return 0;
+}
